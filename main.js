3bf7bb99bfa934ebf5b470c3599e48c37cf572dc
Graham Singer <grahamsinger@grahamsinger.local>
2017-01-05 19:54:32 -0600

    Testing ramda

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var R = require('ramda');
+
+var square = function square(x) {
+    return x * x;
+}
+var squares = R.chain(square, [1, 2, 3, 4, 5]);
+
+document.getElementById('response').innerHTML = squares;
