a5e216fa1008c2f756aee977d288d1d257c47074
Graham Singer <grahamsinger@grahamsinger.local>
2017-01-08 17:35:36 -0600

    minor tweaks

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ var R = require('ramda');
 var square = function square(x) {
     return x * x;
 }
-var squares = R.chain(square, [1, 2, 3, 4, 5]);
+var squares = R.chain(square, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 document.getElementById('response').innerHTML = squares;
